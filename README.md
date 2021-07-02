28e66306ccb1e8fab6398652bf4e18013c9f3cc9
Henry HENG Hian Wee <deezombiedude612@hotmail.com>
2021-07-02 15:00:22 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ci-labs
+ Dr Richard's CI Lab Work
---
+++ lab02-ga.py
@@ -0,0 +1,11 @@
+def value2gray(value):
+  # convert decimal to gray code equivalent
+  return gray
+
+def gray2value(gray):
+  # convert gray code to decimal equivalent
+  return value
+
+if __name__ == "main":
+  print(value2gray(10))
+  print(gray2value("1001"))
